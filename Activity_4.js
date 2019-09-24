c0e8e55cbe565fc6a34169d3b650ddc63662d455
RomanGAnker <54860581+RomanGAnker@users.noreply.github.com>
2019-09-24 00:25:06 -0600

    Create Activity_4.js

---
+++ Activity_4.js
@@ -0,0 +1,38 @@
+// Alternate version of Rock, Paper, Scissors called Bunny, Carrot, Gun
+let Hand = 0
+let Winner = 0
+input.onGesture(Gesture.Shake, function () {
+    Hand = Math.randomRange(0, 10)
+    if (Hand == 0) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    } else if (Hand == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            . # # # .
+            # # . # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . .
+            # # # # #
+            . # # . .
+            . # . . .
+            . # . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    Winner += 1
+    basic.showString("  Winner")
+})
+basic.forever(function () {
+	
+})
